5671f0079700e8a6646f4f2e931125557714a36c
gtq <1244468179@qq.com>
2023-05-25 21:09:12 +0800

    增加readme

--- build/CMakeFiles/onestep_without_delay.dir/DependInfo.cmake
+++ build/CMakeFiles/onestep_without_delay.dir/DependInfo.cmake
@@ -8,16 +8,17 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/gtq/GTQ_pmsm/example/onestep_without_delay.cpp" "CMakeFiles/onestep_without_delay.dir/example/onestep_without_delay.cpp.o" "gcc" "CMakeFiles/onestep_without_delay.dir/example/onestep_without_delay.cpp.o.d"
-  "/home/gtq/GTQ_pmsm/src/PMSM.cpp" "CMakeFiles/onestep_without_delay.dir/src/PMSM.cpp.o" "gcc" "CMakeFiles/onestep_without_delay.dir/src/PMSM.cpp.o.d"
-  "/home/gtq/GTQ_pmsm/src/current_controller.cpp" "CMakeFiles/onestep_without_delay.dir/src/current_controller.cpp.o" "gcc" "CMakeFiles/onestep_without_delay.dir/src/current_controller.cpp.o.d"
-  "/home/gtq/GTQ_pmsm/src/inverter.cpp" "CMakeFiles/onestep_without_delay.dir/src/inverter.cpp.o" "gcc" "CMakeFiles/onestep_without_delay.dir/src/inverter.cpp.o.d"
-  "/home/gtq/GTQ_pmsm/src/speed_control.cpp" "CMakeFiles/onestep_without_delay.dir/src/speed_control.cpp.o" "gcc" "CMakeFiles/onestep_without_delay.dir/src/speed_control.cpp.o.d"
-  "/home/gtq/GTQ_pmsm/src/system.cpp" "CMakeFiles/onestep_without_delay.dir/src/system.cpp.o" "gcc" "CMakeFiles/onestep_without_delay.dir/src/system.cpp.o.d"
+  "D:/codes/GTQ_pmsm/example/onestep_without_delay.cpp" "CMakeFiles/onestep_without_delay.dir/example/onestep_without_delay.cpp.obj" "gcc" "CMakeFiles/onestep_without_delay.dir/example/onestep_without_delay.cpp.obj.d"
+  "D:/codes/GTQ_pmsm/src/Compensator.cpp" "CMakeFiles/onestep_without_delay.dir/src/Compensator.cpp.obj" "gcc" "CMakeFiles/onestep_without_delay.dir/src/Compensator.cpp.obj.d"
+  "D:/codes/GTQ_pmsm/src/PMSM.cpp" "CMakeFiles/onestep_without_delay.dir/src/PMSM.cpp.obj" "gcc" "CMakeFiles/onestep_without_delay.dir/src/PMSM.cpp.obj.d"
+  "D:/codes/GTQ_pmsm/src/current_controller.cpp" "CMakeFiles/onestep_without_delay.dir/src/current_controller.cpp.obj" "gcc" "CMakeFiles/onestep_without_delay.dir/src/current_controller.cpp.obj.d"
+  "D:/codes/GTQ_pmsm/src/inverter.cpp" "CMakeFiles/onestep_without_delay.dir/src/inverter.cpp.obj" "gcc" "CMakeFiles/onestep_without_delay.dir/src/inverter.cpp.obj.d"
+  "D:/codes/GTQ_pmsm/src/speed_control.cpp" "CMakeFiles/onestep_without_delay.dir/src/speed_control.cpp.obj" "gcc" "CMakeFiles/onestep_without_delay.dir/src/speed_control.cpp.obj.d"
+  "D:/codes/GTQ_pmsm/src/system.cpp" "CMakeFiles/onestep_without_delay.dir/src/system.cpp.obj" "gcc" "CMakeFiles/onestep_without_delay.dir/src/system.cpp.obj.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/gtq/GTQ_pmsm
+# Install script for directory: D:/codes/GTQ_pmsm
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/pmsm")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "")
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -27,11 +27,6 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -39,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "C:/MinGW/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/gtq/GTQ_pmsm/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/codes/GTQ_pmsm/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
