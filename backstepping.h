6405ec5058bb95983b65e86eaf3545c9c1b68a91
gtq <1244468179@qq.com>
2023-08-28 09:26:21 +0800

    留坑

--- backstepping.h
+++ backstepping.h
@@ -1,4 +1,25 @@
 #ifndef BACKSTEPPING_H
 #define BACKSTEPPING_H
 
+namespace PanJL
+{
+class backstepping: 
+{
+private:
+    double iq_ref;
+public:
+    backstepping(/* args */);
+    ~backstepping();
+};
+
+backstepping::backstepping(/* args */)
+{
+}
+
+backstepping::~backstepping()
+{
+}
+
+}
+
 #endif
